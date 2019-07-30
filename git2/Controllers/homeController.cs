7e5cc8311da79ef805135eaa97d90b32b563f286
samba2469 <sambasivaeee0215@gmail.com>
2019-07-30 02:58:21 -0700

    new change git 2 7/30

---
+++ git2/Controllers/homeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace git2.Controllers
+{
+    public class homeController : Controller
+    {
+        // GET: home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Insert()
+        {
+            return View();
+        }
+        public ActionResult update()
+        {
+            return View();
+        }
+    }
+}
